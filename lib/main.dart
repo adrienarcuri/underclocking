520f1ee8e854055aefdef0d4c944286ab37ea671
Adrien ARCURI <adrien.arcuri.pro@gmail.com>
2021-02-25 16:05:06 +0100

    Add & init firebase_core

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:underclocking/routes.dart';
 import 'package:underclocking/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  // Initialize Firebase
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.red,
-          brightness: Brightness.dark),
-      home: HomeScreen(),
-      routes: routes(),
-    );
+    return FutureBuilder(
+        // Initialize FlutterFire:
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for erros
+          if (snapshot.hasError) {
+            return Scaffold(
+              body: Center(
+                child: Text('Sorry an error occured during initialization'),
+              ),
+            );
+          }
+          // Once complete, show the app
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MaterialApp(
+              title: 'Flutter Demo',
+              theme: ThemeData(
+                  // This is the theme of your application.
+                  primarySwatch: Colors.red,
+                  brightness: Brightness.dark),
+              home: HomeScreen(),
+              routes: routes(),
+            );
+          } else {
+            return RefreshProgressIndicator();
+          }
+        });
   }
 }
